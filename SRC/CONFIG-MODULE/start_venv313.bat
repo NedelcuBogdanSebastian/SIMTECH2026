d96e7b485889b62ca297301a380b40e1881349cb
Nedelcu Bogdan Sebastian <nedelcu.bogdan.sebastian@gmail.com>
2026-06-08 09:31:03 +0300

    Delete SRC directory

--- SRC/CONFIG-MODULE/start_venv313.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-set VENV_NAME=%USERPROFILE%\AppData\Local\Programs\Python\Python313\venv313
-cmd /k call "%VENV_NAME%\Scripts\activate.bat"
-
-REM We use this only if in the same folder with the venv311
-REM cmd /k call "%~dp0venv311test\Scripts\activate"
